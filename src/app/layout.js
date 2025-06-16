85b363f1cd36b84eb53eb2a6630b3ee6cf2bd4c7
ShuhaiYu <1141071640@qq.com>
2025-06-16 17:15:07 +1000

    fix: improve layout styles for better responsiveness and smooth scrolling

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,17 +12,17 @@ export const LangContext = createContext(null);
 export default function RootLayout({ children }) {
   const [lang, setLang] = useState("en");
   return (
-    <html lang="en">
+    <html lang="en" style={{ scrollBehavior: "smooth" ,scrollbarGutter: "stable"}}>
       <head>
         <title>Austin Edu</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="bg-background text-gray-900">
+      <body className="bg-background text-gray-900 w-full">
         <LangContext.Provider value={{ lang, setLang }}>
           <NavBar />
           {/* 使用固定的容器宽度避免居中计算受影响 */}
-          <main className="flex-1 mt-24 md:mt-32" >
+          <main className="flex-1 mt-24 md:mt-32 " >
             <div className="w-[95%] max-w-screen-2xl mx-auto px-4">{children}</div>
           </main>
           <Toaster />
